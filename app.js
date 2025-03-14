67e6043ddd479af84f7a65a46c5ce319fe73abd8
emma <emmaabryan@gmail.com>
2025-03-14 16:00:47 +0000

    add views

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-import { PrismaClient } from '@prisma/client';
-const path = require("node:path");
-const { Pool } = require("pg");
-const express = require("express");
-const bcrypt = require("bcryptjs")
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 require("dotenv").config();
+
 const prisma = new PrismaClient();
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+    expressSession({
+        cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+        secret: 'a santa at nasa',
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+            checkPeriod: 2 * 60 * 1000,  //ms
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    )
+    })
+);
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
